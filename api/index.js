de2ed64a3fd97a652f2d09c6d9c9aedea6a28a1f
AbuBakar <muhammadabubakar19187@gmail.com>
2025-06-18 13:00:43 +0500

    Created signup page functionality and navigate to signin page

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import connectMongoDB from './db/db.js';
@@ -7,29 +8,29 @@ import connectMongoDB from './db/db.js';
 dotenv.config();
 
 const app = express();
-app.use(express.json())
+
+// Enable CORS
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
+app.use(express.json());
 connectMongoDB();
 
 app.listen(process.env.PORT, () => {
   console.log(`🚀 Server running on PORT ${process.env.PORT}`);
 });
 
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
-app.use("/api/user", userRouter)
-app.use("/api/auth", authRouter)
-
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
     message,
-  })
-})
-
-/**
- 
-* ! 1:22 continue
-
-**/
+  });
+});
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios"
 const SignUp = () => {
+  const [formData, setFormData] = useState({})
+  const navigate = useNavigate()
+  const handleChange = (e) => {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value
+      })
+    }
+    console.log(formData);
+    
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    const res = await axios.post("http://localhost:3000/api/auth/signup", formData);
+    console.log("✅ Signup success:", res.data);
+    navigate("/sign-in")
+  } catch (error) {
+    console.error("❌ Signup error:", error.response?.data || error.message);
+  }
+};
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" placeholder="Username" className='border border-gray-500 bg-white p-3 rounded-lg' id='username'/>
-        <input type="email" placeholder="Email" className='border border-gray-500 p-3 bg-white rounded-lg' id='email'/>
-        <input type="password" placeholder="Password" className='border border-gray-500 p-3 bg-white rounded-lg' id='password'/>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type="text" placeholder="Username" className='border border-gray-500 bg-white p-3 rounded-lg' id='username' onChange={handleChange}/>
+        <input type="email" placeholder="Email" className='border border-gray-500 p-3 bg-white rounded-lg' id='email' onChange={handleChange}/>
+        <input type="password" placeholder="Password" className='border border-gray-500 p-3 bg-white rounded-lg' id='password' onChange={handleChange}/>
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
       </form>
       <div className='flex gap-2 mt-5'>
