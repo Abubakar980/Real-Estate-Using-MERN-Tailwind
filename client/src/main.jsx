48affb72467ff4e554846b1cac554f38f294e1b0
AbuBakar <muhammadabubakar19187@gmail.com>
2025-06-18 23:24:14 +0500

    Add Redux Persisit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,8 @@ import { store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
   </Provider>,
 )
